5ffcff82b82c1d6cc87b5fab2dc21b233d4eb88c
pypye <pentative@gmail.com>
2021-09-27 21:02:04 +0700

    static app demo

--- src/main/java/client/dictionary/Application.java
+++ src/main/java/client/dictionary/Application.java
@@ -10,7 +10,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("application-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 450);
         stage.setTitle("Dictionary");
         stage.setScene(scene);
         stage.show();
