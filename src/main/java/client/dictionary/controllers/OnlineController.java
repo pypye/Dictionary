6e853d9c0bb06722f76e7f1caf9381f0d51d2330
pypye <pentative@gmail.com>
2021-09-30 22:27:38 +0700

    add more feature to client

--- src/main/java/client/dictionary/controllers/OnlineController.java
+++ src/main/java/client/dictionary/controllers/OnlineController.java
@@ -1,8 +1,11 @@
 package client.dictionary.controllers;
 
 import api.GoogleAPI;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 
 import java.io.IOException;
@@ -12,15 +15,28 @@ public class OnlineController {
     private Button offlineButton, onlineButton, translateButton;
     @FXML
     private TextArea inputTextArea, outputTextArea;
+    @FXML
+    private ChoiceBox inputLangChoiceBox, outputLangChoiceBox;
+
+    @FXML
+    private void initialize() {
+        inputLangChoiceBox.setItems(FXCollections.observableArrayList("Vietnam", "English"));
+        outputLangChoiceBox.setItems(FXCollections.observableArrayList("Vietnam", "English"));
+        inputLangChoiceBox.setValue("English");
+        outputLangChoiceBox.setValue("Vietnam");
+    }
 
     @FXML
     protected void onOfflineButtonClick() throws IOException {
         SceneController.switchToOffline();
     }
+
     @FXML
     protected void onOnlineButtonClick() throws IOException {
         SceneController.switchToOnline();
+
     }
+
     @FXML
     protected void onTranslateButtonClick() throws IOException {
         String input = inputTextArea.getText();
